ac7b9422d9cbb4eaa5812459438f415335a1e0d4
Sofiane Ezzehi <ezzehisofiane@gmail.com>
2023-12-09 23:11:19 +0100

    Slight change in comparison methods to display less exples

--- source_code/comparison.py
+++ source_code/comparison.py
@@ -1,11 +1,13 @@
 from source_code.evaluation import *
 
-def plot_dataset(X, h, w, n_data_by_class, title):
+def plot_dataset(X, h, w, n_data_by_class, title, title_save, dataset):
     n_rows = int(np.ceil(n_data_by_class/10))
 
     fig, axs = plt.subplots(n_rows, 10)
     fig.set_size_inches(20, 2.5*n_rows)
     fig.suptitle(title, fontsize=25)
+    # set background color to white
+    fig.patch.set_facecolor('white')
     for i in range(n_data_by_class):
         if n_rows == 1 :
             axs[i].imshow(X[i].reshape(h,w), cmap = "gray")
@@ -16,8 +18,10 @@ def plot_dataset(X, h, w, n_data_by_class, title):
     if n_data_by_class%10 != 0 :
         for i in range(n_data_by_class%10, 10) :
             axs[n_rows-1, i].axis('off')
-
-def compare_methods(dataset, n_data_by_class, beta, seed = 42) :
+    plt.savefig("figures/corruption/" + dataset + "/" + title_save + ".png")
+def compare_methods(dataset, n_data_by_class, beta, n_plot = None, seed = 42) :
+    if n_plot is None :
+        n_plot = n_data_by_class
     np.random.seed(seed)
     n_occult = n_data_by_class
 
@@ -39,18 +43,18 @@ def compare_methods(dataset, n_data_by_class, beta, seed = 42) :
 
     # Plot
     # Original dataset
-    plot_dataset(X_data, h, w, n_data_by_class, title = "Original dataset")
+    plot_dataset(X_data, h, w, n_plot, title = "Original dataset", title_save = "original", dataset = dataset)
     # Occulted dataset
-    plot_dataset(X_occulted, h, w, n_data_by_class, title = "Occulted dataset")
+    plot_dataset(X_occulted, h, w, n_plot, title = "Occulted dataset", title_save = "occulted", dataset = dataset)
     # PCA (beta = 0 in GLPCA)
-    plot_dataset(X_GLPCA_by_beta[0], h, w, n_data_by_class, title = "PCA")
+    plot_dataset(X_GLPCA_by_beta[0], h, w, n_plot, title = "PCA", title_save = "PCA", dataset = dataset)
     # RPCA
-    plot_dataset(L_RPCA, h, w, n_data_by_class, title = "RPCA")
+    plot_dataset(L_RPCA, h, w, n_plot, title = "RPCA", title_save = "RPCA", dataset = dataset)
     # GLPCA
-    plot_dataset(X_GLPCA_by_beta[beta], h, w, n_data_by_class, title = r"GLPCA; $\beta$ = " + str(beta))
+    plot_dataset(X_GLPCA_by_beta[beta], h, w, n_plot, title = r"GLPCA; $\beta$ = " + str(beta), title_save = "GLPCA", dataset = dataset)
     # RGLPCA
-    plot_dataset(X_RGLPCA_by_beta[beta], h, w, n_data_by_class, title = r"RGLPCA; $\beta$ = " + str(beta))
+    plot_dataset(X_RGLPCA_by_beta[beta], h, w, n_plot, title = r"RGLPCA; $\beta$ = " + str(beta), title_save = "RGLPCA", dataset = dataset)
     # OURPCA (RPCA on Graph)
-    plot_dataset(L_OURPCA, h, w, n_data_by_class, title = "RPCA on Graphs")
+    plot_dataset(L_OURPCA, h, w, n_plot, title = "RPCA on Graphs", title_save = "OURPCA", dataset = dataset)
 
     plt.show()
